909237244437c5f2e6617e819ac145c6a00d32cf
Tharindu Rewatha <60292980+TharinduRewatha@users.noreply.github.com>
2020-06-08 16:49:55 +0530

    Create Overload operators.cpp

---
+++ Overload operators.cpp
@@ -0,0 +1,9 @@
+//Tharndu rewatha
+ostream& operator<<(ostream& os, const Complex& c) {
+    return os << c.a << (c.b > 0 ? '+' : '-') << 'i' << c.b;
+}
+
+Complex operator+(const Complex& a, const Complex& b) { 
+    return { a.a + b.a, a.b + b.b };
+}
+//Seekthefreak
