87d7eea9d996b18ebd96ad97b7c44a142780545e
Tharindu Rewatha <60292980+TharinduRewatha@users.noreply.github.com>
2020-06-08 18:08:48 +0530

    Create Preproccessor solution.cpp

---
+++ Preproccessor solution.cpp
@@ -0,0 +1,7 @@
+//Tharindu rewatha
+#define FUNCTION(name, operator) void name(int &current, int candidate){ !(current operator candidate) ? current = candidate : false; }
+#define foreach(v, i) for(int i = 0; i < v.size(); i++)
+#define io(v) cin >> v
+#define INF 10000000
+#define toStr(S) #S
+//seekthefreak
