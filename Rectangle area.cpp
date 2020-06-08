86b3b3836c1d8b2810f0f0d998e5730cd53fa4b1
Tharindu Rewatha <60292980+TharinduRewatha@users.noreply.github.com>
2020-06-08 18:33:02 +0530

    Create Rectangle area.cpp

---
+++ Rectangle area.cpp
@@ -0,0 +1,25 @@
+//Tharindu Rewatha
+class Rectangle
+{
+  protected:
+  int width;
+  int height;
+    public:
+    void read_input()
+    {
+        cin>>width>>height;
+    }
+     void display()
+    {
+    cout<<width<<" "<<height<<"\n";
+    } 
+};
+class RectangleArea :public Rectangle
+{
+    public:
+      void display()
+    {
+      cout<<width * height;
+    }
+};
+//Seekthefreak
