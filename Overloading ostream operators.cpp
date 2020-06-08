2559f550663e86bcce479751b57dae2bf5bf05ca
Tharindu Rewatha <60292980+TharinduRewatha@users.noreply.github.com>
2020-06-08 18:36:16 +0530

    Create Overloading ostream operators.cpp

---
+++ Overloading ostream operators.cpp
@@ -0,0 +1,7 @@
+//Tharindu Rewatha
+ostream& operator<<(ostream& Output, Person& p) {
+  Output << "first_name=" << p.get_first_name() << ","
+         << "last_name=" << p.get_last_name();
+  return Output;
+}
+//seekthefreak
